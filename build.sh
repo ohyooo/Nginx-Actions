3e0d4af0bd0d0d1a656d3e66a7d50ebdcdd309b8
ba la~ ba la~ <8428270+ohyooo@users.noreply.github.com>
2023-04-03 15:06:33 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # https://nginx.org/en/download.html
-NGINX_VERSION=1.23.2
+NGINX_VERSION=1.23.4
 
 # https://www.openssl.org/source/
-OPENSSL_VERSION=3.0.7
+OPENSSL_VERSION=3.0.8
 
 # https://github.com/PCRE2Project/pcre2/releases
 PCRE_VERSION=10.40
